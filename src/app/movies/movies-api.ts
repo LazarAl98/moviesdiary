299d77e40fc49427776cf51cca591d5fc13eae14
Lazar Aleksic <lazaral9998@gmail.com>
2026-09-07 21:14:47 +0200

    Movies servis - dodata metoda toMovie za mapiranje podataka i getPopular za hvatanje filmova

--- src/app/movies/movies-api.ts
+++ src/app/movies/movies-api.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Movie } from './movie.model';
+import { map } from 'rxjs';
 
 interface TmdbMovie {
   id: number;
@@ -26,4 +27,25 @@ export class MoviesApiService {
   });
 
   constructor() {}
+
+  private toMovie(m: TmdbMovie): Movie {
+    return {
+      id: m.id.toString(),
+      title: m.title,
+      overview: m.overview,
+      posterUrl: m.poster_path
+        ? `${environment.tmdbImageUrl}${m.poster_path}`
+        : 'https://placehold.co/342x513?text=No+poster',
+      releaseDate: m.release_date,
+      voteAverage: m.vote_average,
+    };
+  }
+  getPopular() {
+    return this.http
+      .get<TmdbSearchResult>(
+        `${environment.tmdbBaseUrl}/movie/popular?language=en-US&page=1`,
+        { headers: this.headers },
+      )
+      .pipe(map((res) => res.results.map((m) => this.toMovie(m))));
+  }
 }
