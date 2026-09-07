403885ff63681de891a58142325757cb3ad54d95
Lazar Aleksic <lazaral9998@gmail.com>
2026-09-07 20:50:03 +0200

    Login stranica - forma i prijava korisnika

--- src/app/auth/log-in/log-in.page.ts
+++ src/app/auth/log-in/log-in.page.ts
@@ -1,19 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular';
+import { FormsModule, NgForm } from '@angular/forms';
+import {
+  IonButton,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonInput,
+  IonInputPasswordToggle,
+  IonItem,
+  IonLabel,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+  LoadingController,
+} from '@ionic/angular/';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../auth';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.page.html',
   styleUrls: ['./log-in.page.scss'],
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  standalone: true,
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonItem,
+    IonInput,
+    IonButton,
+    IonRow,
+    IonCol,
+    IonGrid,
+    IonInputPasswordToggle,
+    IonLabel,
+    RouterLink,
+  ],
 })
-export class LogInPage implements OnInit {
+export class LogInPage {
+  router: Router = inject(Router);
+  authService: AuthService = inject(AuthService);
+  loadingCtrl: LoadingController = inject(LoadingController);
 
-  constructor() { }
+  async onLogIn(logInForm: NgForm) {
+    if (!logInForm.valid) return;
 
-  ngOnInit() {
+    const loadingEl = await this.loadingCtrl.create({
+      message: 'Prijava u toku...',
+    });
+    await loadingEl.present();
+
+    this.authService.logIn(logInForm.value).subscribe({
+      next: () => {
+        loadingEl.dismiss();
+        this.router.navigateByUrl('/movies/tabs/discover');
+      },
+      error: (err) => {
+        loadingEl.dismiss();
+        console.error(err);
+      },
+    });
   }
-
 }
