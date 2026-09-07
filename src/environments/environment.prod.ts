4bb04faac7341d479f418a95c69c6ce595b11226
Lazar Aleksic <lazaral9998@gmail.com>
2026-09-07 13:23:40 +0200

    Podesen environment fajl (TMDB API)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseAPIKey: 'AIzaSyAjQJhIbAgxIavQt1ccvDr4JWb6TMa_PxA',
+  firebaseRDBUrl:
+    'https://ionic-app-movies-default-rtdb.europe-west1.firebasedatabase.app',
+  tmdbApiKey:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MzlkZDBjOGMyOWIxYTQ4NDcyZjA2OWQwYTdkZmRkOCIsIm5iZiI6MTc2ODMwMjQ0MC4zMjgsInN1YiI6IjY5NjYyNzY4NTkzNWZmYjI3YWE0MzA2ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.leS3rtW7k16iIg7SJD-JDpLXi4oehxrjEt92j7ht8Yo',
+  tmdbBaseUrl: 'https://api.themoviedb.org/3',
+  tmdbImageUrl: 'https://image.tmdb.org/t/p/w342',
 };
