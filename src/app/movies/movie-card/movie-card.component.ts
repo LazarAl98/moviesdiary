f5a447f35f1aeb04d3cb2d5041121c1d0bd93993
Lazar Aleksic <lazaral9998@gmail.com>
2026-09-08 01:07:58 +0200

    Kreirana movie card komponenta

---
+++ src/app/movies/movie-card/movie-card.component.ts
@@ -0,0 +1,33 @@
+import { DecimalPipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonIcon,
+} from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { star } from 'ionicons/icons';
+import { Movie } from '../movie.model';
+
+@Component({
+  selector: 'app-movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.scss'],
+  imports: [
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonIcon,
+    DecimalPipe,
+  ],
+})
+export class MovieCardComponent {
+  @Input() movie!: Movie;
+
+  constructor() {
+    addIcons({ star });
+  }
+}
