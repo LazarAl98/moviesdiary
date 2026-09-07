9b21c21b6621a49e04d5d8ddcd96cd50c673edcc
Lazar Aleksic <lazaral9998@gmail.com>
2026-09-07 20:46:22 +0200

    Kreirana stranica za registraciju - forma i slanje podataka na server

--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -1,19 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  IonButton,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+  LoadingController,
+} from '@ionic/angular/';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../auth';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  standalone: true,
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    ReactiveFormsModule,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonItem,
+    IonInput,
+    IonButton,
+    RouterLink,
+  ],
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
+  router: Router = inject(Router);
+  authService: AuthService = inject(AuthService);
+  loadingCtrl: LoadingController = inject(LoadingController);
 
-  constructor() { }
+  registerForm = new FormGroup({
+    name: new FormControl<string | null>(null, Validators.required),
+    email: new FormControl<string | null>(null, [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: new FormControl<string | null>(null, [
+      Validators.required,
+      Validators.minLength(7),
+    ]),
+  });
 
-  ngOnInit() {
+  async onRegister() {
+    if (this.registerForm.invalid) return;
+
+    const loadingEl = await this.loadingCtrl.create({
+      message: 'Registracija u toku...',
+    });
+    await loadingEl.present();
+
+    this.authService.register(this.registerForm.value as any).subscribe({
+      next: () => {
+        loadingEl.dismiss();
+        this.router.navigateByUrl('/movies/tabs/discover');
+      },
+      error: (err) => {
+        loadingEl.dismiss();
+        console.error(err);
+      },
+    });
   }
-
 }
