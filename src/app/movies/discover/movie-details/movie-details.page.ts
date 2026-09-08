41f74fdf5c6863c0a5bb7832b914fd40b123e8c2
Lazar Aleksic <lazaral9998@gmail.com>
2026-09-08 16:26:45 +0200

    Dodate funkcionalnosti za dodavanje i brisanje filma na watchlisti na stranici movie-details

--- src/app/movies/discover/movie-details/movie-details.page.ts
+++ src/app/movies/discover/movie-details/movie-details.page.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import {
   IonBackButton,
+  IonButton,
   IonButtons,
   IonCard,
   IonCardContent,
@@ -13,16 +14,20 @@ import {
   IonContent,
   IonHeader,
   IonIcon,
+  IonItem,
+  IonLabel,
   IonSpinner,
   IonTitle,
   IonToolbar,
+  ModalController,
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { star } from 'ionicons/icons';
+import { star, checkmarkCircle, timeOutline } from 'ionicons/icons';
 import { Movie } from '../../movie.model';
 import { MoviesApiService } from '../../movies-api';
 import { WatchlistService } from '../../watchlist';
 import { WatchlistItem } from '../../watchlist-item.model';
+import { RatingModalComponent } from '../../rating-modal/rating-modal.component';
 
 @Component({
   selector: 'app-movie-details',
@@ -30,11 +35,15 @@ import { WatchlistItem } from '../../watchlist-item.model';
   styleUrls: ['./movie-details.page.scss'],
   standalone: true,
   imports: [
+    IonBackButton,
     IonHeader,
     IonToolbar,
     IonTitle,
     IonButtons,
-    IonBackButton,
+    IonButton,
+    IonItem,
+    IonLabel,
+    ModalController,
     IonContent,
     IonSpinner,
     IonCard,
@@ -50,14 +59,20 @@ export class MovieDetailsPage implements OnInit, OnDestroy {
   route: ActivatedRoute = inject(ActivatedRoute);
   moviesApi: MoviesApiService = inject(MoviesApiService);
   watchlistService: WatchlistService = inject(WatchlistService);
+  modalCtrl: ModalController = inject(ModalController);
 
   movie?: Movie;
+  watchlistEntry: WatchlistItem | null = null;
   isLoading = false;
 
   private subs = new Subscription();
 
   constructor() {
-    addIcons({ star });
+    addIcons({
+      star,
+      'checkmark-circle': checkmarkCircle,
+      'time-outline': timeOutline,
+    });
   }
 
   ngOnInit() {
@@ -73,11 +88,51 @@ export class MovieDetailsPage implements OnInit, OnDestroy {
             this.isLoading = false;
           }),
         );
+        this.watchlistService.getWatchlist().subscribe();
+        this.subs.add(
+          this.watchlistService.items.subscribe((items) => {
+            this.watchlistEntry = items.find((i) => i.movieId === id) ?? null;
+          }),
+        );
       }),
     );
   }
 
   ngOnDestroy() {
     this.subs.unsubscribe();
+  }
+
+  async onAddOrEdit() {
+    if (!this.movie) return;
+
+    const modal = await this.modalCtrl.create({
+      component: RatingModalComponent,
+      componentProps: {
+        title: this.watchlistEntry ? 'Edit rating' : 'Add to watchlist',
+        myRating: this.watchlistEntry?.myRating ?? null,
+        watched: this.watchlistEntry?.watched ?? false,
+      },
+    });
+
+    modal.present();
+
+    const { data, role } = await modal.onWillDismiss();
+    if (role !== 'confirm' || !this.movie) return;
+
+    const movieId = this.movie.id;
+    const movie = this.movie;
+
+    this.watchlistService.getWatchlist().subscribe((items) => {
+      const existing = items.find((i) => i.movieId === movieId);
+      if (existing) {
+        this.watchlistService
+          .updateItem(existing.id, data.myRating, data.watched)
+          .subscribe();
+      } else {
+        this.watchlistService
+          .addToWatchlist(movie, data.myRating, data.watched)
+          .subscribe();
+      }
+    });
   }
 }
