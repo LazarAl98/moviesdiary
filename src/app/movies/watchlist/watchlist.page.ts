62a269bed937f157aff2cac484eba6891168d689
Lazar Aleksic <lazaral9998@gmail.com>
2026-09-09 12:04:46 +0200

    Napravljeni filteri za stranicu watchlist

--- src/app/movies/watchlist/watchlist.page.ts
+++ src/app/movies/watchlist/watchlist.page.ts
@@ -3,6 +3,7 @@ import { RouterLink } from '@angular/router';
 import { Subscription } from 'rxjs';
 import {
   IonAvatar,
+  IonButtons,
   IonContent,
   IonHeader,
   IonIcon,
@@ -13,15 +14,23 @@ import {
   IonItemSliding,
   IonLabel,
   IonList,
+  IonMenuButton,
+  IonSegment,
+  IonSegmentButton,
+  IonSelect,
+  IonSelectOption,
   IonTitle,
   IonToolbar,
   LoadingController,
   ViewWillEnter,
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { trash } from 'ionicons/icons';
+import { trash, filmOutline } from 'ionicons/icons';
 import { WatchlistService } from '../watchlist';
 import { WatchlistItem } from '../watchlist-item.model';
+
+type WatchFilter = 'all' | 'watched' | 'toWatch';
+type SortOption = 'title' | 'rating';
 
 @Component({
   selector: 'app-watchlist',
@@ -32,25 +41,37 @@ import { WatchlistItem } from '../watchlist-item.model';
     IonHeader,
     IonToolbar,
     IonTitle,
+    IonButtons,
+    IonMenuButton,
     IonContent,
+    IonSegment,
+    IonSegmentButton,
+    IonSelect,
+    IonSelectOption,
     IonList,
     IonItemSliding,
     IonItem,
+    IonItemOptions,
+    IonItemOption,
     IonAvatar,
     IonImg,
     IonLabel,
+    IonIcon,
     RouterLink,
   ],
 })
 export class WatchlistPage implements OnInit, ViewWillEnter, OnDestroy {
   watchlistService: WatchlistService = inject(WatchlistService);
   loadingCtrl: LoadingController = inject(LoadingController);
+
   items: WatchlistItem[] = [];
+  filter: WatchFilter = 'all';
+  sortBy: SortOption = 'title';
 
   private sub = new Subscription();
 
   constructor() {
-    addIcons({ trash });
+    addIcons({ trash, 'film-outline': filmOutline });
   }
 
   ionViewWillEnter() {
@@ -61,6 +82,29 @@ export class WatchlistPage implements OnInit, ViewWillEnter, OnDestroy {
     this.sub = this.watchlistService.items.subscribe((items) => {
       this.items = items;
     });
+  }
+
+  get displayedItems(): WatchlistItem[] {
+    let filtered = this.items;
+    if (this.filter === 'watched') filtered = filtered.filter((i) => i.watched);
+    if (this.filter === 'toWatch')
+      filtered = filtered.filter((i) => !i.watched);
+
+    const sorted = [...filtered];
+    if (this.sortBy === 'rating') {
+      sorted.sort((a, b) => (b.myRating ?? -1) - (a.myRating ?? -1));
+    } else {
+      sorted.sort((a, b) => a.title.localeCompare(b.title));
+    }
+    return sorted;
+  }
+
+  onFilterChange(event: CustomEvent) {
+    this.filter = event.detail.value as WatchFilter;
+  }
+
+  onSortChange(event: CustomEvent) {
+    this.sortBy = event.detail.value as SortOption;
   }
 
   async onRemove(id: string) {
