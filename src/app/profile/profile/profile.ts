7ea3ca0a75efc73ad2a8dbbb5656aa0c54af1286
Lazar Aleksic <lazaral9998@gmail.com>
2026-09-09 10:59:56 +0200

    Promena na profile servisu - azuriranje profila (ime i bio)

--- src/app/profile/profile/profile.ts
+++ src/app/profile/profile/profile.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { BehaviorSubject, map, tap } from 'rxjs';
+import { BehaviorSubject, map, switchMap, take, tap } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { AuthService } from '../../auth/auth';
 import { UserProfile } from '../user-profile.model';
@@ -29,5 +29,26 @@ export class ProfileService {
         map((data) => data ?? EMPTY_PROFILE),
         tap((profile) => this._profile.next(profile)),
       );
+  }
+
+  //azuriranje ime i bio, avatar ostaje kakav je bio
+  updateProfile(name: string, bio: string) {
+    return this.saveProfile((current) => ({ ...current, name, bio }));
+  }
+
+  private saveProfile(merge: (current: UserProfile) => UserProfile) {
+    const userId = this.authService.getUserId();
+    return this.profile.pipe(
+      take(1),
+      switchMap((current) => {
+        const updated = merge(current ?? EMPTY_PROFILE);
+        return this.http
+          .put<UserProfile>(
+            `${environment.firebaseRDBUrl}/users/${userId}.json?auth=${this.authService.getToken()}`,
+            updated,
+          )
+          .pipe(tap(() => this._profile.next(updated)));
+      }),
+    );
   }
 }
