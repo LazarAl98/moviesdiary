473114c146faebde0c4c58d932c7fe8a86ff6e51
Lazar Aleksic <lazaral9998@gmail.com>
2026-09-09 11:44:00 +0200

    Dodat html kod na profile stranici i implementirana metoda onSave za menjanje informacija

--- src/app/profile/profile/profile.page.ts
+++ src/app/profile/profile/profile.page.ts
@@ -1,20 +1,84 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonMenuButton,
+  IonTextarea,
+  IonTitle,
+  IonToolbar,
+  ToastController,
+  ViewWillEnter,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { personCircle } from 'ionicons/icons';
+import { Subscription } from 'rxjs';
+import { ProfileService } from './profile';
+import { UserProfile } from '../user-profile.model';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonButtons,
+    IonMenuButton,
+    IonContent,
+    IonIcon,
+    IonItem,
+    IonInput,
+    IonTextarea,
+    IonButton,
+    FormsModule,
+  ],
 })
-export class ProfilePage implements OnInit {
+export class ProfilePage implements OnInit, ViewWillEnter, OnDestroy {
+  profileService: ProfileService = inject(ProfileService);
+  toastCtrl: ToastController = inject(ToastController);
 
-  constructor() { }
+  profile: UserProfile | null = null;
 
-  ngOnInit() {
+  private sub = new Subscription();
+
+  constructor() {
+    addIcons({ 'person-circle': personCircle });
   }
 
+  ionViewWillEnter() {
+    this.profileService.getProfile().subscribe();
+  }
+
+  ngOnInit() {
+    this.sub = this.profileService.profile.subscribe((profile) => {
+      this.profile = profile;
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  onSave(form: NgForm) {
+    if (form.invalid) return;
+
+    this.profileService
+      .updateProfile(form.value.name, form.value.bio ?? '')
+      .subscribe(async () => {
+        const toast = await this.toastCtrl.create({
+          message: 'Profil sačuvan.',
+          duration: 1500,
+          color: 'success',
+        });
+        await toast.present();
+      });
+  }
 }
